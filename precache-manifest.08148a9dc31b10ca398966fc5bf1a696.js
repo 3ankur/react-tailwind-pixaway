24e4626b4dd5df258b927c7adb49fc9d71ad9f48
root <3ankur.v@gmail.com>
2020-05-24 19:46:44 +0530

    Updates

---
+++ precache-manifest.08148a9dc31b10ca398966fc5bf1a696.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "465e68a7e99a5cb37c5d34122ae677ae",
+    "url": "/react-tailwind-pixaway/index.html"
+  },
+  {
+    "revision": "9b56b0b518b65aefa4c9",
+    "url": "/react-tailwind-pixaway/static/css/main.d8e9d6c7.chunk.css"
+  },
+  {
+    "revision": "da45c17db7fae51c563d",
+    "url": "/react-tailwind-pixaway/static/js/2.0da99863.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-tailwind-pixaway/static/js/2.0da99863.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b56b0b518b65aefa4c9",
+    "url": "/react-tailwind-pixaway/static/js/main.50305d38.chunk.js"
+  },
+  {
+    "revision": "a558b53689750e427899",
+    "url": "/react-tailwind-pixaway/static/js/runtime-main.094ab171.js"
+  }
+]);
