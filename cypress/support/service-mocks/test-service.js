aa816276e0d91115d4ccadf3a7061ae28b47fce1
Ankur Vishakarma <ankur.vishwakarma@sky.uk>
2020-08-01 16:09:57 +0100

    Removed commented code.

--- cypress/support/service-mocks/test-service.js
+++ cypress/support/service-mocks/test-service.js
@@ -9,10 +9,8 @@ export const HomePage = {
     const { status = 200, scenario = 'success' } = options;
     const response = `fixture:${SERVICE}/get_${scenario}.json`;
 console.log(response);
-    //  url: `https://cdn.privacy-mgmt.com/consent/tcfv${tcfVersion}/consent/v3/**`,
     return cy.route({
-     // url: `${endPoint}/**`,
-     url:`https://pixabay.com/api/?key=16706699-0a39087c67e883c7f93b62962&q=&image_type=photo&page=1`,
+      url: `${endPoint}/**`,
       method: 'GET',
       response,
       status,
